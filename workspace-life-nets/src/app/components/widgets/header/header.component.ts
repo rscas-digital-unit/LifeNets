23e5d3aac140a6c82434b34ca8824efd5cce0928
Antonio Pistilli <antonio.pistilli@eui.eu>
2026-04-20 14:17:56 +0200

    Added css/html fix

--- workspace-life-nets/src/app/components/widgets/header/header.component.ts
+++ workspace-life-nets/src/app/components/widgets/header/header.component.ts
@@ -20,4 +20,27 @@ export class HeaderComponent {
       document.body.classList.remove('going-down');
     }
   }
+
+  scrollTo(event: Event, targetId: string): void {
+    event.preventDefault();
+
+    const element = document.getElementById(targetId);
+    if (!element) return;
+
+    const offset = 100; // altezza header
+
+    const y =
+      element.getBoundingClientRect().top +
+      window.pageYOffset -
+      offset;
+
+    window.scrollTo({
+      top: y,
+      behavior: 'smooth'
+    });
+  }
 }
+
+
+
+
